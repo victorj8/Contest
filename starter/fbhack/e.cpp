6d4efad02c17d5bc1fd6403101f05aa2213ae892
victorj8 <victor.j8@gmail.com>
2018-07-21 17:25:29 -0500

    added files for fb hacker cup

---
+++ starter/fbhack/e.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+int main(){
+    int test;
+    scanf("%d", &test);
+    for(int t=0;t<test;t++) {
+        printf("Case #%d: ",t+1);
+    }
+
+    return 0;
+}
