4223bb234d557e30e65dbce2313f451e4ebec2a4
Victor Camacho <victor.camacho@block.one>
2020-09-26 20:27:28 -0400

    add some misc function related to mod

--- utils/miscellaneous.cpp
+++ utils/miscellaneous.cpp
@@ -1,3 +1,5 @@
+// Hash for unordered_set
+
 struct pair_hash {
     inline std::size_t operator()(const std::pair<int,int> & v) const  {
         return v.first*31+v.second;
@@ -5,3 +7,7 @@ struct pair_hash {
 };
 
 unordered_set<pair<int,int>, pair_hash> s;
+
+// Modulus of positive / negative numbers
+
+int mod(int a, int b) { return (a % b + b) % b; }
