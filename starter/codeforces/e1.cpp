d1974c344a5e3022b388c18f056b90646c9aa75f
Victor Camacho <victor.camacho@block.one>
2021-04-23 10:13:57 -0400

    add more source files for possible different versions

---
+++ starter/codeforces/e1.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
